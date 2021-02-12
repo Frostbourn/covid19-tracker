d39366fa8b61d4678111e0eca1e99bbcda6c5ee1
Frostbourn <frostbourn@gmail.com>
2021-02-12 16:08:34 +0100

    News API update

--- src/api/index.js
+++ src/api/index.js
@@ -2,59 +2,6 @@ import axios from "axios";
 
 const url = "https://api.coronatracker.com/v3/stats/worldometer";
 
-// export const fetchData = async (country) => {
-//   let changableUrl = url;
-
-//   if (country) {
-//     changableUrl = `${url}/country?countryCode=${country}`;
-//   } else {
-//     changableUrl = `${url}/global`;
-//   }
-
-//   try {
-//     const {
-//       data: {
-//         totalConfirmed,
-//         totalDeaths,
-//         totalRecovered,
-//         totalNewCases,
-//         totalNewDeaths,
-//         totalActiveCases,
-//         created
-//       }
-//     } = await axios.get(changableUrl);
-
-//     const defaultData = {
-//       totalConfirmed,
-//       totalDeaths,
-//       totalRecovered,
-//       totalNewCases,
-//       totalNewDeaths,
-//       totalActiveCases,
-//       created
-//     };
-//     return defaultData;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// export const fetchDailyData = async () => {
-//   try {
-//     const { data } = await axios.get(`https://covid19.mathdro.id/api/daily`);
-
-//     const modifiedDailyData = data.map((dailyData) => ({
-//       confirmed: dailyData.confirmed.total,
-//       dailyConfirmed: dailyData.deltaConfirmed,
-//       deaths: dailyData.deaths.total,
-//       date: dailyData.reportDate
-//     }));
-//     return modifiedDailyData;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
 export const fetchCountries = async (countryCode, countryName) => {
   try {
     const [global, topCountries, country, dailyData, news] = await axios.all([
@@ -63,9 +10,9 @@ export const fetchCountries = async (countryCode, countryName) => {
       axios.get(`${url}/country?countryCode=${countryCode}`),
       axios.get(`${url}/totalTrendingCases?limit=100`),
       axios.get(
-        `https://api.coronatracker.com/news/trending?limit=5&offset=45&country=${
-          countryName ? countryName : "USA"
-        }`
+        `https://api.coronatracker.com/news/trending?limit=5&&offset=0&countryCode=${countryCode}&country=${
+          countryName ? countryName : "United+States"
+        }&language=en`
       )
     ]);
 
